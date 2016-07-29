bb4f164c7e9a5d39163082502d95646813664252
Rodrigo Gregori <spam@gregori.eti.br>
2016-07-29 16:57:34 -0300

    Working client

--- src/br/org/catolicasc/programacaovi/TimeClient.java
+++ src/br/org/catolicasc/programacaovi/TimeClient.java
@@ -5,6 +5,9 @@
  */
 package br.org.catolicasc.programacaovi;
 
+import br.org.catolicasc.programacaovi.timeservice.TimeService;
+import br.org.catolicasc.programacaovi.timeservice.TimeServiceImplService;
+
 /**
  *
  * @author Rodrigo
@@ -15,7 +18,10 @@ public class TimeClient {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        TimeServiceImplService ts = new TimeServiceImplService();
+        TimeService s = ts.getTimeServiceImplPort();
+        System.out.println("Time elapsed: " + s.getTimeAsElapsed());
+        System.out.println("Time string: " + s.getTimeAsString());
     }
     
 }
